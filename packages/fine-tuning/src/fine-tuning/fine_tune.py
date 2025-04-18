32e9e14a8ebe095020941a357a4607e42085a326
Shafiq Ninaba <shafiq_ninaba@aiap.sg>
2025-04-18 21:43:49 +0800

    feat: hydra config and logging setup

--- packages/fine-tuning/src/fine-tuning/fine_tune.py
+++ packages/fine-tuning/src/fine-tuning/fine_tune.py
@@ -1,161 +1,26 @@
-from unsloth import FastLanguageModel
-import torch
-from loguru import logger
-from unsloth.chat_templates import get_chat_template
-from datasets import load_dataset
-from trl import SFTTrainer
-from transformers import TrainingArguments, DataCollatorForSeq2Seq
-from unsloth import is_bfloat16_supported
-from unsloth.chat_templates import standardize_sharegpt
-from unsloth.chat_templates import train_on_responses_only
-
-# setup logging to save to ../../logs/fine_tuning.log rotated on file size 10 MB
-logger.add("../../logs/fine_tuning.log", rotation="10 MB", level="INFO")
-logger.info("Starting fine-tuning script.")
-
-max_seq_length = 2048  # Choose any! We auto support RoPE Scaling internally!
-dtype = (
-    None  # None for auto detection. Float16 for Tesla T4, V100, Bfloat16 for Ampere+
-)
-load_in_4bit = True  # Use 4bit quantization to reduce memory usage. Can be False.
-
-# 4bit pre quantized models we support for 4x faster downloading + no OOMs.
-fourbit_models = [
-    "unsloth/Meta-Llama-3.1-8B-bnb-4bit",  # Llama-3.1 2x faster
-    "unsloth/Meta-Llama-3.1-8B-Instruct-bnb-4bit",
-    "unsloth/Meta-Llama-3.1-70B-bnb-4bit",
-    "unsloth/Meta-Llama-3.1-405B-bnb-4bit",  # 4bit for 405b!
-    "unsloth/Mistral-Small-Instruct-2409",  # Mistral 22b 2x faster!
-    "unsloth/mistral-7b-instruct-v0.3-bnb-4bit",
-    "unsloth/Phi-3.5-mini-instruct",  # Phi-3.5 2x faster!
-    "unsloth/Phi-3-medium-4k-instruct",
-    "unsloth/gemma-2-9b-bnb-4bit",
-    "unsloth/gemma-2-27b-bnb-4bit",  # Gemma 2x faster!
-    "unsloth/Llama-3.2-1B-bnb-4bit",  # NEW! Llama 3.2 models
-    "unsloth/Llama-3.2-1B-Instruct-bnb-4bit",
-    "unsloth/Llama-3.2-3B-bnb-4bit",
-    "unsloth/Llama-3.2-3B-Instruct-bnb-4bit",
-    "unsloth/Llama-3.3-70B-Instruct-bnb-4bit",  # NEW! Llama 3.3 70B!
-]  # More models at https://huggingface.co/unsloth
-
-model, tokenizer = FastLanguageModel.from_pretrained(
-    model_name="unsloth/Llama-3.2-3B-Instruct",  # or choose "unsloth/Llama-3.2-1B-Instruct"
-    max_seq_length=max_seq_length,
-    dtype=dtype,
-    load_in_4bit=load_in_4bit,
-    # token = "hf_...", # use one if using gated models like meta-llama/Llama-2-7b-hf
-)
+# from unsloth import FastLanguageModel
+# import torch
+# from unsloth.chat_templates import get_chat_template
+# from datasets import load_dataset
+# from trl import SFTTrainer
+# from transformers import TrainingArguments, DataCollatorForSeq2Seq
+# from unsloth import is_bfloat16_supported
+# from unsloth.chat_templates import standardize_sharegpt
+# from unsloth.chat_templates import train_on_responses_only
+import logging
+from omegaconf import OmegaConf, DictConfig
+import hydra
 
 
-# add LoRA to the model
-model = FastLanguageModel.get_peft_model(
-    model,
-    r=16,  # Choose any number > 0 ! Suggested 8, 16, 32, 64, 128
-    target_modules=[
-        "q_proj",
-        "k_proj",
-        "v_proj",
-        "o_proj",
-        "gate_proj",
-        "up_proj",
-        "down_proj",
-    ],
-    lora_alpha=16,
-    lora_dropout=0,  # Supports any, but = 0 is optimized
-    bias="none",  # Supports any, but = "none" is optimized
-    # [NEW] "unsloth" uses 30% less VRAM, fits 2x larger batch sizes!
-    use_gradient_checkpointing="unsloth",  # True or "unsloth" for very long context
-    random_state=3407,
-    use_rslora=False,  # We support rank stabilized LoRA
-    loftq_config=None,  # And LoftQ
-)
+@hydra.main(version_base=None, config_path="../../conf", config_name="config")
+def main(cfg: DictConfig) -> None:
+    logging_config = OmegaConf.to_container(cfg.logging, resolve=True)
+
+    # Initialize logging
+    logging.config.dictConfig(logging_config)
+    logger = logging.getLogger(__name__)
+    logger.info("Starting fine-tuning script.")
 
 
-tokenizer = get_chat_template(
-    tokenizer,
-    chat_template="llama-3.1",
-)
-
-
-def formatting_prompts_func(examples):
-    convos = examples["conversations"]
-    texts = [
-        tokenizer.apply_chat_template(
-            convo, tokenize=False, add_generation_prompt=False
-        )
-        for convo in convos
-    ]
-    return {
-        "text": texts,
-    }
-
-
-dataset = load_dataset("mlabonne/FineTome-100k", split="train")
-
-
-dataset = standardize_sharegpt(dataset)
-dataset = dataset.map(
-    formatting_prompts_func,
-    batched=True,
-)
-
-
-trainer = SFTTrainer(
-    model=model,
-    tokenizer=tokenizer,
-    train_dataset=dataset,
-    dataset_text_field="text",
-    max_seq_length=max_seq_length,
-    data_collator=DataCollatorForSeq2Seq(tokenizer=tokenizer),
-    dataset_num_proc=2,
-    packing=False,  # Can make training 5x faster for short sequences.
-    args=TrainingArguments(
-        per_device_train_batch_size=2,
-        gradient_accumulation_steps=4,
-        warmup_steps=5,
-        # num_train_epochs = 1, # Set this for 1 full training run.
-        max_steps=60,
-        learning_rate=2e-4,
-        fp16=not is_bfloat16_supported(),
-        bf16=is_bfloat16_supported(),
-        logging_steps=1,
-        optim="adamw_8bit",
-        weight_decay=0.01,
-        lr_scheduler_type="linear",
-        seed=3407,
-        output_dir="outputs",
-        report_to="none",  # Use this for WandB etc
-    ),
-)
-
-
-trainer = train_on_responses_only(
-    trainer,
-    instruction_part="<|start_header_id|>user<|end_header_id|>\n\n",
-    response_part="<|start_header_id|>assistant<|end_header_id|>\n\n",
-)
-
-# @title Show current memory stats
-gpu_stats = torch.cuda.get_device_properties(0)
-start_gpu_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
-max_memory = round(gpu_stats.total_memory / 1024 / 1024 / 1024, 3)
-logger.info(f"GPU = {gpu_stats.name}. Max memory = {max_memory} GB.")
-logger.info(f"{start_gpu_memory} GB of memory reserved.")
-
-trainer_stats = trainer.train()
-
-# @title Show final memory and time stats
-used_memory = round(torch.cuda.max_memory_reserved() / 1024 / 1024 / 1024, 3)
-used_memory_for_lora = round(used_memory - start_gpu_memory, 3)
-used_percentage = round(used_memory / max_memory * 100, 3)
-lora_percentage = round(used_memory_for_lora / max_memory * 100, 3)
-logger.info(f"{trainer_stats.metrics['train_runtime']} seconds used for training.")
-logger.info(
-    f"{round(trainer_stats.metrics['train_runtime']/60, 2)} minutes used for training."
-)
-logger.info(f"Peak reserved memory = {used_memory} GB.")
-logger.info(f"Peak reserved memory for training = {used_memory_for_lora} GB.")
-logger.info(f"Peak reserved memory % of max memory = {used_percentage} %.")
-logger.info(f"Peak reserved memory for training % of max memory = {lora_percentage} %.")
-
-model.save_pretrained_gguf("../../data/model", tokenizer, quantization_method="q4_k_m")
+if __name__ == "__main__":
+    main()
